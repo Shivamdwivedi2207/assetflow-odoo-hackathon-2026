f035c1d5ee3dd839bc0189430db2a4bccbb42b5d
Shivamdwivedi2207 <dwivedishivam@gmail.com>
2026-07-12 10:56:00 +0530

    Create AssetFlow module foundation

---
+++ assetflow/__manifest__.py
@@ -0,0 +1,36 @@
+{
+    "name": "AssetFlow",
+    "version": "1.0.0",
+    "summary": "Enterprise Asset & Resource Management System",
+    "description": """
+        AssetFlow
+        Enterprise Asset & Resource Management System
+        Developed for Odoo Hackathon 2026.
+    """,
+
+    "author": "Team AssetFlow",
+    "website": "https://github.com/Shivamdwivedi2207/assetflow-odoo-hackathon-2026",
+
+    "category": "Operations",
+
+    "license": "LGPL-3",
+
+    "depends": [
+        "base",
+        "mail"
+    ],
+
+    "data": [
+
+    ],
+
+    "demo": [
+
+    ],
+
+    "installable": True,
+
+    "application": True,
+
+    "auto_install": False,
+}
---
+++ assetflow/models/__init__.py
@@ -0,0 +1,6 @@
+from . import department
+from . import employee
+from . import asset_category
+from . import asset
+from . import allocation
+from . import maintenance
